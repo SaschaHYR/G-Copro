f822e1b49c4617cc52c77ef9d592b4e746a507be
[dyad] <alex.hayer@gmail.com>
2026-01-19 23:27:27 +0100

    [dyad] Fixing React not defined error - wrote 1 file(s)

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Badge } from '@/components/ui/badge';

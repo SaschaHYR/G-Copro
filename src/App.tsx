9867e1bdb18b1c08b9ed9ba38921fc5c985e5e26
[dyad] <alex.hayer@gmail.com>
2026-01-21 22:43:20 +0100

    [dyad] Fixed QueryClient error by adding QueryClientProvider - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,35 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './components/AuthProvider';
 import { NotificationProvider } from './components/NotificationContext';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Login from './pages/Login';
 import Index from './pages/Index';
 import Admin from './pages/Admin';
 import ProtectedRoute from './components/ProtectedRoute';
 import './globals.css';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
     <Router>
       <AuthProvider>
         <NotificationProvider>
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/" element={
-              <ProtectedRoute>
-                <Index />
-              </ProtectedRoute>
-            } />
-            <Route path="/admin" element={
-              <ProtectedRoute requiredRole="Superadmin">
-                <Admin />
-              </ProtectedRoute>
-            } />
-          </Routes>
+          <QueryClientProvider client={queryClient}>
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/" element={
+                <ProtectedRoute>
+                  <Index />
+                </ProtectedRoute>
+              } />
+              <Route path="/admin" element={
+                <ProtectedRoute requiredRole="Superadmin">
+                  <Admin />
+                </ProtectedRoute>
+              } />
+            </Routes>
+          </QueryClientProvider>
         </NotificationProvider>
       </AuthProvider>
     </Router>
